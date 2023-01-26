6c6c82af18a275ec8a12ff5fad9295e00365521c
Joseph Pecoraro <jpecoraro342@yahoo.com>
2023-01-26 16:28:07 -0500

    Initial Commit

---
+++ SubtitleMerge/SubtitleMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubtitleMergeApp.swift
+//  SubtitleMerge
+//
+//  Created by Joseph Pecoraro on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SubtitleMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
